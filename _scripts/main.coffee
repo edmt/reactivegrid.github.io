18774cebcc9129c6201a1dfcb29a19c8b98a37bf
Adrian Ayala <adrian.ayala17@gmail.com>
2014-03-09 18:28:13 -0600

    Adds a script to make the menu dynamic

--- _scripts/main.coffee
+++ _scripts/main.coffee
@@ -1,1 +1,6 @@
-console.log 'Hello world!'
+title = document.title.split(' | ')
+
+switch title[1]
+  when undefined then $('#home').addClass('active')
+  when 'Archive' then $('#archive').addClass('active')
+  when 'Team' then $('#team').addClass('active')
--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+var title;
+
+title = document.title.split(' | ');
+
+switch (title[1]) {
+  case void 0:
+    $('#home').addClass('active');
+    break;
+  case 'Archive':
+    $('#archive').addClass('active');
+    break;
+  case 'Team':
+    $('#team').addClass('active');
+}
